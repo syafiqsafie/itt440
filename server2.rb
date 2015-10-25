3fa0db30651c70e88446c37d0c9b08c257f7d35c
amirulsyafiq <syafiqsafie19@gmail.com>
2015-10-25 23:43:42 +0800

    Assignment 2 ruby

---
+++ server2.rb
@@ -0,0 +1,13 @@
+require 'socket'
+
+PORT = 3010
+socket = TCPServer.new('0.0.0.0', PORT)
+
+optval = socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF);
+optval = optval.unpack "i"
+puts "SNDBUF Early Option : #{optval}"
+socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF, 5342);
+optval = socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF);
+optval = optval.unpack "i"
+puts "SNDBUF After Edit : #{optval}"
+
